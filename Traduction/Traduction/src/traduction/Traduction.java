8f4a49c971e8e9736bc6a335368671e5cfd97f87
sopact <sopact@gmail.com>
2015-11-26 16:55:51 +0100

    Code refait version du 26_11_15 16h55

--- Traduction/Traduction/src/traduction/Traduction.java
+++ Traduction/Traduction/src/traduction/Traduction.java
@@ -5,9 +5,6 @@
 */
 package traduction;
 
-import java.io.*;
-import java.util.StringTokenizer;
-
 
 /**
  *
@@ -15,84 +12,12 @@ import java.util.StringTokenizer;
  */
 public class Traduction {
     
-    private String fichierFiacre;
-    private BufferedReader reader;
-    
-    /**
-     * Methode permettant de récupérer un bloc de texte du fichier écrit en Fiacre.
-     * Le but est donc d'identifier tout les types de blocs et les traduire.
-     * Les différents blocs sont : 
-     * - Initialisation d'un process
-     * - Transition
-     * - Etats (States)
-     * 
-     * @param fichierFiacre
-     * @return
-     * @throws FileNotFoundException
-     * @throws IOException 
-     */
-    
-    public String RecupererBloc (String fichierFiacre) throws FileNotFoundException,IOException{
-        BufferedReader fichier = new BufferedReader(new FileReader(fichierFiacre));
-        String ligne = fichier.readLine();
-        while (ligne!="end"){ // TO DO analyser tout les types de bloc et faire un switch
-            AnalyserBloc(ligne); // TO DO analyser un paragraphe
-            ligne=fichier.readLine();
-        }
-        return ligne;
+    public static void main(String[] args) {
+        TraductionBloc trd = new TraductionBloc();
+        String[] bloc = trd.RecupererBloc("test.txt");
+        String trad = trd.AnalyserBloc(bloc);
+        System.out.println(trad);
     }
-    
-/**
- * Methode permettant d'analyser un bloc de language Fiacre.
- * Le but est d'identifer chaque marqueur (if, while, from, to...) puis traduire la condition dans un mot.
- * Chaque ligne est ensuite ajouté à un bloc.
- * On écrira chaque bloc les uns à la suite des autres dans un fichier .cts .
- * @param ligne
- * @return 
- */
-    
-    public String AnalyserBloc (String ligne){
-        // TODO découper le paragraphe en ligne
-        String bloc="";
-        String delimiteurs = " ,.;";
-        StringTokenizer tokenizer = new StringTokenizer (ligne,delimiteurs);
-        
-        String debut = tokenizer.nextToken();
-        
-        while (debut!=null){ //TODO revoir
-            
-            switch (debut) {
-                
-                case "from" :
-                    String States1 = tokenizer.nextToken();
-                    String suite = tokenizer.nextToken();
-                    switch (suite) {
-                        
-                        case "if" :
-                            
-                            break;
-                            
-                        case "while" :
-                            break;
-                            
-                        case "to" :
-                            String States2 = tokenizer.nextToken();
-                            
-                            String debutDeclarationTransition = "transition [intermediate {"+States1+"'"+States1+"="+States1+"'"+States1+" - 1;}]";
-                            String finDeclarationTransition = "{"+States1+"'"+States1+"="+States1+"'"+States1+" - 1,"+States2+"'"+States2+"="+States2+"'"+States2+" + 1;}";
-                            
-                            break;
-                            
-                        default :
-                            suite = tokenizer.nextToken();
-                            
-                            
-                    }
-                    break;
-                    
-            }
-        }
-        return bloc;
-    }
-    
 }
+    
+    
--- Traduction/Traduction/src/traduction/TraductionBloc.java
+++ Traduction/Traduction/src/traduction/TraductionBloc.java
@@ -13,58 +13,74 @@ import java.util.StringTokenizer;
  *
  * @author PORET Quentin
  */
-public class Traduction {
-    
-    private String fichierFiacre;
-    private BufferedReader reader;
+public class TraductionBloc {
     
     /**
-     * Methode permettant de récupérer un bloc de texte du fichier écrit en Fiacre.
+     *
+     */
+    public TraductionBloc (){
+    }
+    
+    /**
+     * Méthode permettant de récupérer un bloc de texte du fichier écrit en Fiacre.
      * Le but est donc d'identifier tout les types de blocs et les traduire.
-     * Les différents blocs sont : 
+     * Les différents blocs sont :
      * - Initialisation d'un process
      * - Transition
      * - Etats (States)
-     * 
+     * Un bloc est un ensemble de ligne qui est stocké dans un tableau de mots.
      * @param fichierFiacre
      * @return
-     * @throws FileNotFoundException
-     * @throws IOException 
      */
-    
-    public String RecupererBloc (String fichierFiacre) throws FileNotFoundException,IOException{
-        BufferedReader fichier = new BufferedReader(new FileReader(fichierFiacre));
-        String ligne = fichier.readLine();
-        while (ligne!="end"){ // TO DO analyser tout les types de bloc et faire un switch
-            AnalyserBloc(ligne); // TO DO analyser un paragraphe
-            ligne=fichier.readLine();
+    public String[] RecupererBloc(String fichierFiacre){
+        try{
+            BufferedReader fichier = new BufferedReader(new FileReader(fichierFiacre));
+            int j=0;
+            String[] ligne = new String[3]; // TODO Changer le tableau en List ou autre
+            ligne[j] = fichier.readLine();
+            while (!"end".equals(ligne[j])){ // TO DO analyser tout les types de bloc et faire un switct
+                j++;
+                ligne[j]=fichier.readLine();
+                
+            }
+            // String bloc = AnalyserBloc(ligne);
+            return ligne;
         }
-        return ligne;
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
     
-/**
- * Methode permettant d'analyser un bloc de language Fiacre.
- * Le but est d'identifer chaque marqueur (if, while, from, to...) puis traduire la condition dans un mot.
- * Chaque ligne est ensuite ajouté à un bloc.
- * On écrira chaque bloc les uns à la suite des autres dans un fichier .cts .
- * @param ligne
- * @return 
- */
+    /**
+     * La méthode AnalyserBloc prend un entrée un bloc (tableau de mots)
+     * Le premier mot sert à connaître le type de bloc (transition, process etc).
+     * Ensuite on détecte différents mots clés et on traduit le reste de la ligne en fonction
+     * A la fin on récupère tout les bouts de code et on les assemble dans un bloc de texte
+     * @param ligne
+     * @return
+     */
     
-    public String AnalyserBloc (String ligne){
+    public String AnalyserBloc (String ligne[]){
         // TODO découper le paragraphe en ligne
-        String bloc="";
+        String bloc = null;
         String delimiteurs = " ,.;";
-        StringTokenizer tokenizer = new StringTokenizer (ligne,delimiteurs);
+        
+        
+        StringTokenizer tokenizer = new StringTokenizer (ligne[0],delimiteurs);
         
         String debut = tokenizer.nextToken();
         
-        while (debut!=null){ //TODO revoir
+        String debutDeclarationTransition = "";
+        String finDeclarationTransition = "";
+        
+        
+        switch (debut) {
             
-            switch (debut) {
-                
-                case "from" :
-                    String States1 = tokenizer.nextToken();
+            case "from" :
+                String States1 = tokenizer.nextToken();
+                for (int j=1;j<ligne.length;j++){
+                    tokenizer = new StringTokenizer(ligne[j],delimiteurs);
                     String suite = tokenizer.nextToken();
                     switch (suite) {
                         
@@ -78,21 +94,24 @@ public class Traduction {
                         case "to" :
                             String States2 = tokenizer.nextToken();
                             
-                            String debutDeclarationTransition = "transition [intermediate {"+States1+"'"+States1+"="+States1+"'"+States1+" - 1;}]";
-                            String finDeclarationTransition = "{"+States1+"'"+States1+"="+States1+"'"+States1+" - 1,"+States2+"'"+States2+"="+States2+"'"+States2+" + 1;}";
-                            
+                            debutDeclarationTransition = "transition [intermediate {"+States1+"'"+States1+"="+States1+"'"+States1+" - 1;}]";
+                            finDeclarationTransition = "{"+States1+"'"+States1+"="+States1+"'"+States1+" - 1,"+States2+"'"+States2+"="+States2+"'"+States2+" + 1;}";
                             break;
                             
-                        default :
-                            suite = tokenizer.nextToken();
+                            
                             
                             
                     }
-                    break;
                     
-            }
+                    
+                }
+                break;
+            default :
+                System.out.println("erreur");
         }
+        
+        bloc = debutDeclarationTransition + "When (a'a>=1)" + finDeclarationTransition;
         return bloc;
     }
-    
 }
+
